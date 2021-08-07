18306f684cc03a62bd61b729f01abd62360e9e84
zhiyi <muyizz@yeah.net>
2021-08-08 00:16:27 +0800

    create:简单Java类的字节码分析
    无变量、四则运算、if、for

---
+++ Week1/src/main/java/org/zhiyi/coursecode/week1/Hello.java
@@ -0,0 +1,32 @@
+package org.zhiyi.coursecode.week1;
+
+public class Hello {
+
+    // public static final String staticFinalString = "zhiyi";
+    // public final String finalString = "zhiyi";
+    // public static String staticString = "zhiyi";
+    private String fieldA;
+
+    public static void main(String[] args) {
+
+        // final String finalString1 = "zhiyi";
+        
+        // byte b = 'A';
+        // short s = 1;
+        // int a = 10;
+        // float f = 2F;
+        // double d = 2.222;
+        // long c = 1L;
+        // char ch = 'C';
+        // boolean bo = true;
+
+        // if (bo) {
+        //     for (int i = 0; i < a; i++) {
+        //         f++;
+        //         d++;
+        //         c++;
+        //     }
+        // }
+
+    }
+}
